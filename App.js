fada5233e88a02a59b736c280f7e3605a2a76c5b
plmn65410 <69616757+plmn65410@users.noreply.github.com>
2020-10-20 15:12:16 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export default function app(){
+  return(
+    <View style={{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+      }}>
+      <Text>ไอ้เพชร ไอ้โง่</Text>
+    </View>
+  )
+}
